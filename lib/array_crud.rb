0455c68a20cf5a0e7199f3989281c2c436f04920
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-15 19:54:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,7 +11,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  ["this","is","where","it"].shift("wow")
+  ["this","is","where","it"].unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
