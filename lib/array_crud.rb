883471776151ca6ac12eecefe9801af7ca8bab58
Franklyn Rodriguez <franklynr92@gmail.com>
2020-04-15 19:58:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,7 +24,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  ["wow","is","am","it"]
+ arr=["wow","is","am","it"]
+ arr
 end
 
 def retrieve_first_element_from_array(array)
